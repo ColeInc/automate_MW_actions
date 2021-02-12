be33d2ed023a622e77c96e508a92fdcf93ce31c5
cole.mcconnell@fbu.com <cole.mcconnell@fbu.com>
2021-02-12 17:19:37 +1300

    finished underlying csv file transformation function

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 automates a tedious task for me of re-processing failed records
-
-![](https://giphy.com/embed/AwNGX4XvvODO8)
--- app/automate_MW_actions.py
+++ app/automate_MW_actions.py
@@ -1,9 +1,15 @@
 import os
+import datetime
+
 
 # ----------------------------------------------------------------------
-# Define exports directory here (remember a / on the end of the path buddy):
+# Define directories here! (remember a / on the end of the path buddy):
 # ----------------------------------------------------------------------
 exports_dir = 'C:/dev/Cole/Project Notes/Laminex Ecommerce - MW Actions/@ Python Script/exports_dir/'
+output_dir = 'C:/dev/Cole/Project Notes/Laminex Ecommerce - MW Actions/@ Python Script/output_dir/'
+archive_dir = 'C:/dev/Cole/Project Notes/Laminex Ecommerce - MW Actions/@ Python Script/archive_dir/'
+# ----------------------------------------------------------------------
+
 
 def list_files_in_exports_dir():
     # get a list of all filenames currently inside the unprocessed export.csv directory.
@@ -15,26 +21,92 @@ def list_files_in_exports_dir():
 
 
 def transform_exports_csv(filename):
-    # for each file found inside list_files_in_exports_dir, now read this file, and return its contents.
+    # for each file found inside list_files_in_exports_dir, transform its contents to the necessary output file.
 
     print("Reading contents of file --> {}".format(filename))
 
-    if os.path.isfile(exports_dir + filename):
-        with open(exports_dir + filename) as file:
-            print(file.read())
-            # data = json.load(file)
-    else:
-        print("{} not found!".format(filename))
-        return None
+    try:
+        if os.path.isfile(exports_dir + filename):
+            with open(exports_dir + filename) as file:
+                contents = file.read()
+                file.close()
+                # print(file.read())
+
+                ### removing first line "CORRELATION_ID,ORDERNUMBER_PAYLOAD,PAYLOAD" from file contents:
+                removed_first_line = contents.replace("CORRELATION_ID,ORDERNUMBER_PAYLOAD,PAYLOAD", "")
+                print("Removed first line: CORRELATION_ID,ORDERNUMBER_PAYLOAD,PAYLOAD!")
+
+                ### removing all line feeds and carrage returns from file:
+                removed_line_endings = removed_first_line.replace("\n", "")
+
+                ### replacing soap body opening and closing tags + adding newline char onto closing tag:
+                opening_current = '<soap-env:Body xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://fbu.com/CommonServices/GenericAuditService">'
+                opening_new = '<soapenv:Body xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:lam="http://fbu.com/BuildingProducts/LaminexEcomSampleProductOrchestrator" xmlns:com="http://fbu.com/common">'
+                replaced_openings = removed_line_endings.replace(opening_current, opening_new)
+                print("Replaced soap body openings!")
+
+                closing_current = '</soap-env:Body>'
+                closing_new ='</soapenv:Body>\n'
+                replaced_closings = replaced_openings.replace(closing_current, closing_new)
+                print("Replaced soap body closings!")
+
+                ### checking for extra commas which break the request separators. should only have 2 per line:
+
+                # replaced_closings = 'hello, there, my, name, is, cole, haha' # delete me
+
+                final_removed_commas = ""
+                line_num = 1
+                for line in replaced_closings.splitlines():
+                    comma_count = line.count(",")
+                    if comma_count <= 2:
+                        final_removed_commas += line + "\n"
+                    else:
+                        print("\nExtra commas found in line_number {} !".format(line_num))
+                        print("Number of extra commas --> {}".format(comma_count-2))
+                        split_line = line.split(",")
+                        removed_commas = ""
+                        for i in range(len(split_line)):
+                            if i < 2:
+                                removed_commas += (split_line[i] + ",")
+                            else:
+                                removed_commas += split_line[i]
+                        final_removed_commas += removed_commas + "\n"
+                        # print("\ncommas were found in this part of the line:\n\n", ",".join(split_line[2:len(split_line)]), sep="")
+                        print("Commas were found in this part of the line:\n\n", split_line[len(split_line)-2], ",", split_line[len(split_line)-1][0:20], "\n", sep="")
+                    line_num += 1
+
+                ### write file contents to output file in output_dir:
+
+                now = datetime.datetime.now()
+                export_file_name = now.strftime("export_%Y%m%d_%H%M%S.csv")
+
+                with open(output_dir + export_file_name, 'w') as file:
+                    file.write(replaced_closings)
+                    file.close()
+
+                ### move successfully transformed file to archive_dir:
+                os.replace(exports_dir + filename, archive_dir + filename)
+
+        else:
+            print("Could not find file with name --> {}".format(filename))
+            return None
+
+    except Exception as e:
+        print("------------------------------------------------------")
+        print("Error while transforming csv file:\n", e)
+        print("------------------------------------------------------")
 
 
 def main():
+    print("------------------------------------------------------")
     print("Starting automate_MW_actions.py...")
+    print("------------------------------------------------------")
+
     # file_list = list_files_in_exports_dir()
 
     # for filename in file_list:
     #     transform_exports_csv(filename)
 
-    transform_exports_csv('cats.txt')
+    transform_exports_csv('cats2.csv')
 
 main()
