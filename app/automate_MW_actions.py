8b60445e8413f3983b864b79817eec258f08ee7e
cole.mcconnell@fbu.com <cole.mcconnell@fbu.com>
2021-03-08 17:24:02 +1300

    first working version of phone, postcode, and commas replacement

--- app/automate_MW_actions.py
+++ app/automate_MW_actions.py
@@ -53,29 +53,47 @@ def transform_exports_csv(filename):
 
                 # replaced_closings = 'hello, there, my, name, is, cole' # delete me
                 final_removed_commas = ""
+                yes_list = ['yes', 'ya', 'yep', 'yes pls', 'yas', 'ye', 'y', 'yes!']
+                no_list = ['no', 'n', 'nah', 'na', 'nope', 'no thanks']
                 line_num = 1
                 for line in replaced_closings.splitlines():
 
                     ### Checking for valid phone numbers:
 
-                    line = 'this is a string where the substring "<ns2:phone>0407445979x</ns2:phone>" is repeated several <ns2:phoneNumber>yeet</ns2:phoneNumber> times'
+                    line = 'this is a test line <ns2:postalCode>3427</ns2:postalCode> where their <ns2:phone>0407445979x</ns2:phone>are many <ns2:postalCode>cole</ns2:postalCode> values, and<ns2:phoneNumber/> this <ns2:postalCode/> sne<ns2:phoneNumber>yeet</ns2:phoneNumber>aky'
+                    # line = 'this is a test line <ns2:postalCode>3427</ns2:postalCode> where their are many <ns2:postalCode>cole</ns2:postalCode> values, and this <ns2:postalCode/> sneaky'
+                    # line = 'this is a string where the substring "<ns2:phone>0407445979x</ns2:phone>" is repeated several <ns2:phoneNumber>yeet</ns2:phoneNumber> times'
                     # line = 'this is a string where the substring "<ns2:phone/>" is repeated several <ns2:phoneNumber/>yeet</ns2:phoneNumber> times'
                     # print([(a.start(), a.end()) for a in list(re.finditer('<ns2:phone', line))])
                     # print([(line[a.start():a.start()+30]) for a in list(re.finditer('<ns2:phone', line))])
 
+                    # for each instance we find "<ns2:phone" in the line:
+                    # edited_phone_line = ''
+                    phone_offset = 0
                     for a in list(re.finditer('<ns2:phone', line)):
+
+                        # use edited line if it has a value in it, otherwise just stick with original line value.
+                        # if len(edited_phone_line) > 0:
+                        #     line = edited_phone_line
+                        #     for b in list(re.finditer('<ns2:phone', edited_phone_line)):
+                        #         a = b
+                        #         break
+                        # may need to offload all uses of line variable below to another variable, so that the original line value isn't getting manipulated each iteration
+                        # print("line getting used: ", line)
+
                         # If we find an empty phone field, do nothing, otherwise fetch the value + its indexes from inbetween the tags:
-                        if line[a.start():a.start()+12] == "<ns2:phone/>":
+                        if line[a.start()-phone_offset:a.start()+12-phone_offset] == "<ns2:phone/>":
                             print("empty phone found, skipping") # delete
 
-                        elif line[a.start():a.start()+18] == "<ns2:phoneNumber/>":
+                        elif line[a.start()-phone_offset:a.start()+18-phone_offset] == "<ns2:phoneNumber/>":
                             print("empty phoneNumber found, skipping") # delete
 
                         else:
-                            print("line preview: ", line[a.start():a.start()+35])
-                            rest_of_line = line[a.start()+1:-1] # the + 1 removes first < from very start
+                            rest_of_line = line[a.start()-phone_offset+1:] # the + 1 removes first < from very start
                             value_opening_index = rest_of_line.find(">")
                             value_closing_index = rest_of_line.find("<")
+                            original_opening_index = a.start()-phone_offset + value_opening_index + 2 # will have to calibrate these 1-2 indexes each way when replacing
+                            original_closing_index = a.start()-phone_offset + value_closing_index + 1 # will have to calibrate these 1-2 indexes each way when replacing
 
                             if (value_opening_index != -1) and (value_closing_index != -1):
                                 phone_value = rest_of_line[value_opening_index+1:value_closing_index]
@@ -86,21 +104,45 @@ def transform_exports_csv(filename):
                             if phone_value.isdigit(): # if it contains all numbers, its fine, let it slide
                                 pass
                             else: # else, has some invalid characters, then we do the following:
+                                # print("line preview: ", line[a.start():a.start()+35])
                                 print("Invalid phone number found:\n--->", phone_value, "<---", sep="")
-                                requested_phone_value = input("what you want to change this field to fam?: ")
-                                # probably have a final confirmation prompt like:
-                                print("Are you sure you want to change the <ns2:phone field to:\n--->", requested_phone_value, "<---", sep="")
-                                confirmation = input("yes/no: ")
 
-                                # if confirmation in yes_list:
-                                #     final_phone_value = requested_phone_value
-                                #     # insert this back into final line value
-                                # elif confirmation in no_list:
-                                #
+                                current_value_length = len(phone_value)
+                                final_phone_value = ''
+                                valid = False
+                                while not valid:
+                                    requested_phone_value = input("what you want to change this field to buddy?: ")
+                                    if len(requested_phone_value) > 0:
+                                        print("Are you sure you want to change the <ns2:phone field to:\n--->", requested_phone_value, "<---", sep="")
+                                        confirmation = input("yes/no: ")
+                                        if (confirmation in yes_list) and requested_phone_value.isdigit():
+                                            final_phone_value = requested_phone_value
+                                            new_value_length = len(requested_phone_value)
+                                            phone_offset = current_value_length - new_value_length + phone_offset
+                                            valid = True
+                                        elif (confirmation in yes_list) and requested_phone_value.isdigit() == False:
+                                            print("Invalid phone number entered sorry, has to be numbers only.")
+                                        elif confirmation in no_list:
+                                            pass # takes us back to another while loop iteration asking for new input.
+                                    else:
+                                        confirmation = input("no input given! are you sure you want no value in this field? yes/no: ")
+                                        if confirmation in yes_list:
+                                            # if the value i entered is fully blank, need to replace the line with <ns2:phone/>... or i guess <ns2:phone></ns2:phone> would work
+                                            final_phone_value = '' # set final_phone_value as blank
+                                            new_value_length = len(requested_phone_value)
+                                            phone_offset = current_value_length - new_value_length + phone_offset
+                                            valid = True
+                                        elif confirmation in no_list:
+                                            pass # takes us back to another while loop iteration asking for new input.
+
+                                # now have to replace the original value in this line with the new content we want added.
+                                line = line[0:original_opening_index] + final_phone_value + line[original_closing_index:]
+                                # print("first half:", line[0:original_opening_index])
+                                # print("end half:", line[original_closing_index:])
+                                print("NEW line preview: ", line[a.start():a.start()+35])
+                                print("FULL new_line: ", line)
 
 
-
-                            # pass
 
 
                     # for each line, loop through it and print all instances of <ns2:phone, etc.
@@ -124,12 +166,94 @@ def transform_exports_csv(filename):
                     # name = input("yes/no: ")
                     # now have to replace the original value in this line with the new content we want added.
 
-
+                    # line = edited_phone_line
 
                     ### Checking for valid postcodes:
 
-                    # pass
+                    # for each instance we find "<ns2:post" in the line:
 
+                    # edited_postcode_line = ''
+                    postcode_offset = 0
+
+                    for a in list(re.finditer('<ns2:post', line)):
+
+                        # use edited line if it has a value in it, otherwise just stick with original line value.
+                        # if len(edited_postcode_line) > 0:
+                        #     line = edited_postcode_line
+                        #     for b in list(re.finditer('<ns2:post', edited_postcode_line)):
+                        #         a = b
+                        #         break
+                        # may need to offload all uses of line variable below to another variable, so that the original line value isn't getting manipulated each iteration
+                        # print("line getting used: ", line)
+
+                        # If we find an empty postcode field, do nothing, otherwise fetch the value + its indexes from inbetween the tags:
+                        if line[a.start()-postcode_offset:a.start()+17-postcode_offset] == "<ns2:postalCode/>":
+                            print("empty postcode found, skipping") # delete
+                        else:
+                            print("line preview: ", line[a.start()-postcode_offset:a.start()+35-postcode_offset])
+
+                            # print("BEFORE rest_of_line: ", line[a.start():])
+                            rest_of_line = line[a.start()-postcode_offset+1:] # the + 1 removes first < from very start
+                            # print("rest_of_line: ", rest_of_line)
+                            value_opening_index = rest_of_line.find(">")
+                            value_closing_index = rest_of_line.find("<")
+                            original_opening_index = a.start()-postcode_offset + value_opening_index + 2
+                            original_closing_index = a.start()-postcode_offset + value_closing_index + 1
+
+                            if (value_opening_index != -1) and (value_closing_index != -1):
+                                postcode_value = rest_of_line[value_opening_index+1:value_closing_index]
+                            else:
+                                error_msg = "Invalid closing and opening >'s found when looking at <ns2:post!!! very bad!"
+                                raise Exception(error_msg)
+
+                            if postcode_value.isdigit(): # if it contains all numbers, its fine, let it slide
+                                print("valid postcode found, letting it past")
+                                pass
+                            else: # else, has some invalid characters, then we do the following:
+                                # print("line preview: ", line[a.start()+postcode_offset:a.start()+35+postcode_offset])
+                                print("Invalid postcode found:\n--->", postcode_value, "<---", sep="")
+
+                                current_value_length = len(postcode_value)
+                                postcode_value = ''
+                                valid = False
+                                while not valid:
+                                    postcode_value = input("what you want to change this field to buddy?: ")
+                                    if len(postcode_value) > 0:
+                                        print("Are you sure you want to change the <ns2:post field to:\n--->", postcode_value, "<---", sep="")
+                                        confirmation = input("yes/no: ")
+                                        if (confirmation in yes_list) and postcode_value.isdigit():
+                                            postcode_value = postcode_value
+                                            new_value_length = len(postcode_value)
+                                            postcode_offset = current_value_length - new_value_length + postcode_offset
+                                            valid = True
+                                        elif (confirmation in yes_list) and postcode_value.isdigit() == False:
+                                            print("Invalid postcode entered sorry, has to be numbers only.")
+                                        elif confirmation in no_list:
+                                            pass # takes us back to another while loop iteration asking for new input.
+                                    else:
+                                        confirmation = input("no input given! are you sure you want no value in this field? yes/no: ")
+                                        if confirmation in yes_list:
+                                            # if the value i entered is fully blank, need to replace the line with <ns2:post/>... or i guess <ns2:postalCode></ns2:postalCode> would work
+                                            postcode_value = '' # set postcode_value as blank
+                                            new_value_length = len(postcode_value)
+                                            postcode_offset = current_value_length - new_value_length + postcode_offset
+                                            valid = True
+                                        elif confirmation in no_list:
+                                            pass # takes us back to another while loop iteration asking for new input.
+
+                                # now have to replace the original value in this line with the new content we want added.
+                                line = line[0:original_opening_index] + postcode_value + line[original_closing_index:]
+                                # print("first half:", line[0:original_opening_index])
+                                # print("end half:", line[original_closing_index:])
+                                # print("NEW postcode line preview: ", edited_postcode_line[a.start():a.start()+35])
+                                print("FULL new_line: ", line)
+
+
+
+
+
+                    # line = edited_postcode_line # final line value after postcode section
+                    # print("FINAL LINE:", line)
 
                     ### checking for extra commas which break the request separators. should only have 2 per line:
 
@@ -154,18 +278,19 @@ def transform_exports_csv(filename):
                     line_num += 1
 
 
+                    # here we actually have to write each individual edited line back into the file... idk how to do that.
 
 
                 # ### write file contents to output file in output_dir:
-                #
-                # now = datetime.datetime.now()
-                # export_file_name = now.strftime("export_%Y%m%d_%H%M%S%f.csv")
-                #
-                # with open(output_dir + export_file_name, 'w') as file:
-                #     file.write(replaced_closings)
-                #     file.close()
-                #
-                # ### move successfully transformed file to archive_dir:
+
+                now = datetime.datetime.now()
+                export_file_name = now.strftime("export_%Y%m%d_%H%M%S%f.csv")
+
+                with open(output_dir + export_file_name, 'w') as file:
+                    file.write(final_removed_commas)
+                    file.close()
+
+                ### move successfully transformed file to archive_dir:
                 # os.replace(input_dir + filename, archive_dir + filename)
 
         else:
